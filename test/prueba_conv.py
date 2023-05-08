658563c03a5b59aa69edce8c304c484d3545353c
David R <davidrubio97@outlook.com>
2023-05-08 20:45:36 +0200

    update

--- test/prueba_conv.py
+++ test/prueba_conv.py
@@ -1,17 +1,63 @@
-x = [0.5, 2, 0, 0, 0, 0]
-h = [1, 1, 1, 0, 0, 0, 0]
+import numpy as np
+import matplotlib.pyplot as plt
 
-for i in range(len(x)):
-    c=0
-    for j in range(len(h)-1):
-        print("x[",j, "] = ", x[j])
-        
-        if i-j < 0:
-            g = 0
-            print("h[", i-j, "] = ", h[g])
-            
-        else:
-            g = i-j
-            print("h[", i-j, "] = ", h[g])
-        c += x[j]*h[g]
+x = [1,1,1,1,1,1]
+h = np.array([0,0,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1])
+
+x = [1,2,1,1]
+h=np.array([1, -1, 0, 0, 1, 1])
+
+x= [5, 4, 3, 2, 1, 0]
+h=np.array([3, 2, 1])
+
+
+x= [0, 1, 2, 3, 4]
+h = np.array([0,0,0,0,0,1,1,1,1,1,1,1,1,1, 1, 1, 1,1 , 1, 1, 1,1 ])
+
+
+input = list(np.zeros(len(h)))
+
+graph = []
+
+for i in range(24):
+
+    input = list(input)
+    if i < len(x):
+        input.insert(0, x[i])
+    else:
+        input.insert(0, 0)
+    input.pop(-1)
+    input = np.array(input)
+    
+    c = input*h
+    suma = sum(c)
+
     print(c)
+    print(suma)
+
+    graph.append(suma)
+    
+plt.stem(graph)
+plt.show()
+
+
+
+
+    # for j in range(len(h)-1):
+    #     print("x[",j, "] = ", input[j])
+        
+
+
+    #     if i-j < 0:
+    #         g = 0
+    #         print("h[", i-j, "] = ", h[g])
+            
+    #     else:
+    #         g = i-j
+    #         print("h[", i-j, "] = ", h[g])
+
+
+    #     c += x[j]*h[g]
+
+
+    # print(c)
